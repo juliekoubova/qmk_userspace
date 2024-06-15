d3b14f33628e7ddf915aa157dd2b9b75176e931d
Julie Koubova <julie@koubova.net>
2024-06-15 22:10:34 +0200

    gpl

--- users/juliekoubova/vim/perform_action.c
+++ users/juliekoubova/vim/perform_action.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 (c) Julie Koubova (julie@koubova.net)
+/* Copyright 2024 (c) Julie Koubova (julie@koubova.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
