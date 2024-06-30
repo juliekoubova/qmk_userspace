e23db56327695f799ff53c4a89ab0b20ce8de7e7
Julie Koubova <julie@koubova.net>
2024-06-30 15:05:32 +0200

    fix

--- users/juliekoubova/rules.mk
+++ users/juliekoubova/rules.mk
@@ -1,7 +1,6 @@
 ifeq ($(strip $(VIM_MODE_ENABLE)), yes)
   SRC += vim/pending.c
   SRC += vim/perform_action.c
-  SRC += vim/sequence.c
   SRC += vim/statemachine.c
   SRC += vim/vim.c
   SRC += vim/vim_mode.c
