1195f96cdc3e013f42db4b471e83f9f3a481a694
Julie Koubova <julie@koubova.net>
2024-08-13 23:10:14 +0200

    vim

--- keyboards/crkbd/keymaps/juliekoubova/config.h
+++ keyboards/crkbd/keymaps/juliekoubova/config.h
@@ -30,6 +30,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define TAPPING_TOGGLE 2
+
+#define LAYER_STATE_8BIT
+#define NO_MUSIC_MODE
+
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
--- keyboards/crkbd/keymaps/juliekoubova/keymap.c
+++ keyboards/crkbd/keymaps/juliekoubova/keymap.c
@@ -17,13 +17,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "vim.h"
+
+enum key_codes {
+    QK_VIM = SAFE_RANGE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      QK_VIM,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -68,3 +73,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+void keyboard_post_init_user(void) {
+#ifdef VIM_DEBUG
+    debug_enable = true;
+#endif
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return process_record_vim(keycode, record, QK_VIM);
+}
+
+void housekeeping_task_user() {
+    vim_task();
+}
